e4988c8e6e5bd49459b47bc1fd7d5646e7f6c31d
kalay <iinkklapad@gmail.com>
2025-03-15 13:28:53 +0800

    initial commit - get input

--- index.js
+++ index.js
@@ -1,9 +1,64 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
-const app = express();
 
+const app = express();
+const port = 3000;
+const API_URL = "https://api.openweathermap.org";
+
+//Own bearer token.
+const yourBearerToken = "63f5b9ae84c15ba0960d1e37283d9fe9";
+const config = {
+  headers: { Authorization: `Bearer ${yourBearerToken}` },
+};
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "Waiting for data..." });
+});
+
+app.post("/put-secret", async (req, res) => {
+  const searchId = req.body.id;
+  try {
+    const result = await axios.put(
+      API_URL + "/secrets/" + searchId,
+      req.body,
+      config
+    );
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 app.set('view engine', 'ejs');
+
+
+
+
 
 const cities = [
   { name: 'New York', lat: 40.7128, lon: -74.0060 },
@@ -43,3 +98,4 @@ app.get('/weather/:cityName', (req, res) => {
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
+*/
